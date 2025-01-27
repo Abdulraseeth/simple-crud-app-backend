9e9e04deb4026f7bf94744355aabf38a1ea275a9
Abdul Raseeth <abdulraseeth04@gmail.com>
2025-01-27 16:42:18 +0530

    cors origin imported

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Product = require("./models/product.model.js");
+const cors = require("cors"); // Import cors
 const productRoute = require("./routes/product.route.js");
 const app = express();
 
+
 // middleware
+app.use(cors()); // Allow cross-origin requests
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
@@ -12,17 +14,13 @@ app.use(express.urlencoded({extended: false}));
 // routes
 app.use("/api/products", productRoute);
 
-
-
-
-
 mongoose
   .connect(
 "mongodb+srv://abdulraseeth04:4djyXUKOgj2eOcTC@backenddb.j30zj.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB"
   )
   .then(() => {
     console.log("Connected to database!");
-    app.listen(3000, () => {
+    app.listen(3001, () => {
       console.log("Server is running on port 3000");
     });
   })
